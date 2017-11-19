00cc9f213330cb0056c886272c08945e0df60f8e
Cheng Gibson <chgibson@thoughtworks.com>
2017-11-19 17:42:55 +0800

    Extract directory creation code into method + test

--- enron_email_parser/save_to_disk_util.py
+++ enron_email_parser/save_to_disk_util.py
@@ -5,8 +5,12 @@ import pickle
 
 
 def save_to_disk(path, data_dict, protocol=-1):
-    # creates directories recursively for saving file
-    pathlib.Path(os.path.dirname(path)).mkdir(parents=True, exist_ok=True)
+    __create_directories_to_path(path)
 
     with gzip.open(path, 'wb') as f:
         pickle.dump(data_dict, f, protocol)
+
+
+def __create_directories_to_path(path):
+    dirname = os.path.dirname(path)
+    pathlib.Path(dirname).mkdir(parents=True, exist_ok=True)
---
+++ enron_email_parser/test/test_save_to_disk_util.py
@@ -0,0 +1,31 @@
+from unittest import TestCase
+from unittest.mock import patch, MagicMock
+
+import callee
+
+from enron_email_parser import save_to_disk_util
+
+
+class TestSaveToDiskUtil(TestCase):
+    @patch('pickle.dump')
+    @patch('gzip.open')
+    @patch('pathlib.Path')
+    @patch('os.path.dirname')
+    def test_save_to_disk(self,
+                          mock_os_path_dirname,
+                          mock_pathlib_path,
+                          mock_gzip_open,
+                          mock_pickle_dump):
+        mock_path = MagicMock()
+        mock_os_path_dirname.return_value = "/dirname"
+        mock_pathlib_path.return_value = mock_path
+
+        some_dict = {"some-key": "some-value"}
+        some_full_path = "/dirname/filename"
+
+        save_to_disk_util.save_to_disk(some_full_path, some_dict, -1)
+
+        mock_os_path_dirname.assert_called_with(some_full_path)
+        mock_pathlib_path.assert_called_with("/dirname")
+        mock_path.mkdir.assert_called_once()
+        mock_pickle_dump.assert_called_with(some_dict, callee.Any(), -1)
